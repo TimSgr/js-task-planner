b9d8b78c06a437dc372cf966644f20d14d9a2b88
TimSgr <timseeger2000@gmail.com>
2024-01-05 23:46:07 +0100

    add expandable details option

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,12 @@ const allTasksBox = document.querySelector(".all_tasks");
 const newTaskDescription_wrapper = document.querySelector(".new_task_wrapper .input_fields .input_field_task_description");
 const newTaskDescription = document.querySelector(".new_task_wrapper .input_fields .input_field_task_description #taskdescription");
 const newTaskName = document.querySelector(".new_task_wrapper .input_fields .input_field_task_name #taskname");
+const closeIcon = document.querySelector(".new_task_wrapper .close_icon");
+const datepicker = document.querySelector('#datepicker');
+
+closeIcon.addEventListener('click', function(){
+    newTaskWrapper.style.display = "none";
+});
 
 function initializeTaskCreation() {
     newTaskName.addEventListener("keyup", () => {
@@ -26,11 +32,14 @@ allTasksBox.addEventListener('click', function(event) {
         removeTask(event.target);
     } else if (event.target.classList.contains('complete')) {
         completeTask(event.target);
+    } else if (event.target.classList.contains('new_task_name')){
+        expand_minimize_details(event.target);
     }
 });
 
+
 function removeTask(element) {
-    const parent = element.parentElement;
+    const parent = element.parentElement.parentElement;
     parent.remove();
 }
 
@@ -39,20 +48,44 @@ function completeTask(element) {
     parent.classList.add("completed");
 }
 
+function expand_minimize_details(element){
+    const parent = element.parentElement.parentElement;
+    if(parent.classList.contains('expanded')){
+        parent.classList.remove("expanded");
+    }else{
+        parent.classList.add("expanded");
+    }
+}
+
 function submitNewTask() {
     if (newTaskName) {
         newTaskWrapper.style.display = "none";
         allTasksBox.style.paddingTop = "0px";
-        const taskId = `task-${Math.random().toString(36).substring(2, 9)}`;
+        const wholeTask = document.createElement('div');
+        wholeTask.className = "whole_task";
         const taskElement = document.createElement('div');
-        taskElement.id = taskId;
         taskElement.className = 'task';
         taskElement.innerHTML = `
             <span class='delete'>X</span>
             <span class="new_task_name">${newTaskName.value}</span>
             <span class='complete'>✔</span>
         `;
-        allTasksBox.appendChild(taskElement);
+        const taskDetails = document.createElement('div');
+        taskDetails.style.maxHeight="0px";
+        taskDetails.style.display="none";
+        taskDetails.className = 'task_details';
+
+        taskDetails.innerHTML = `
+            <div class="">
+                <span class="">${datepicker.value}</span>  
+            </div>
+            <div class="">
+                <span class="">${newTaskDescription.value}</span>
+            </div>
+        `;
+        wholeTask.appendChild(taskElement);
+        wholeTask.appendChild(taskDetails);
+        allTasksBox.appendChild(wholeTask);
         newTaskName.value = "";
         newTaskDescription.value = "";
         newTaskDescription_wrapper.style.display = "none";
@@ -62,3 +95,18 @@ function submitNewTask() {
 
 // Initialize the task creation process
 initializeTaskCreation();
+
+function validate() {
+    if (document.querySelector('.input_field_task_checkbox #duedate').checked) {
+        datepicker.style.display="block";
+        var today = new Date();
+        var dd = String(today.getDate()).padStart(2, '0');
+        var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        var yyyy = today.getFullYear();
+        today = yyyy + '-' + mm + '-' + dd;
+        datepicker.value=today;
+    } else {
+        datepicker.style.display="none";
+    }
+}
+
